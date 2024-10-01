458aaf8d4ad396eb0219437ddd66af2d707efdcf
binaryman <binaryman21@gmail.com>
2024-10-01 16:35:03 -0300

    cambio del diseño del sitio

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 window.onload = (event) => {
-
-  };
-
-
   fetch('code_ex/C/ejPunterosRobot.c')
     .then(res => res.text())
     .then(res => {
       document.getElementById("ejPunterosRobot").innerHTML = res;
       $('#ejPunterosRobot').each(function(i, e) {hljs.highlightBlock(e);})
-      copyToClipboard();
+      
     }
-    )
+    )  
+  };
 
-    function copyToClipboard() {
-     
-    var copyText = document.getElementById("ejPunterosRobot");
-    
-    navigator.clipboard.writeText(copyText.textContent);
 
-    
-    }
+
+
+  
