e4b892f62cff8c833d9f066613aaa32d5203edb0
binaryman <binaryman21@gmail.com>
2024-10-01 15:11:04 -0300

    copiar al portapapeles

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.onload = (event) => {
-    
+
   };
 
 
@@ -8,5 +8,15 @@ window.onload = (event) => {
     .then(res => {
       document.getElementById("ejPunterosRobot").innerHTML = res;
       $('#ejPunterosRobot').each(function(i, e) {hljs.highlightBlock(e);})
+      copyToClipboard();
     }
     )
+
+    function copyToClipboard() {
+     
+    var copyText = document.getElementById("ejPunterosRobot");
+    
+    navigator.clipboard.writeText(copyText.textContent);
+
+    
+    }
