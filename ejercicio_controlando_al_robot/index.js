6e2b2e027f7e9be8175776bd06f99e89339daf8c
binaryman <binaryman21@gmail.com>
2024-10-01 23:09:53 -0300

    a

---
+++ ejercicio_controlando_al_robot/index.js
@@ -0,0 +1,15 @@
+window.onload = (event) => {
+  fetch('../code_ex/C/ejPunterosRobot.c')
+    .then(res => res.text())
+    .then(res => {
+      document.getElementById("ejPunterosRobot").innerHTML = res;
+      $('#ejPunterosRobot').each(function(i, e) {hljs.highlightBlock(e);})
+      
+    }
+    )  
+  };
+
+
+
+
+  
